d0cb9850e42f1441b13bb588c0c2ec3534ea5e0b
hackingyseguridad <antonio_taboada@hotmail.es>
2020-09-28 12:19:23 +0200

    Create instalar.sh

---
+++ instalar.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "instalando ngrok .."
+sudo apt update && sudo apt full-upgrade -y
+sudo apt install wget zip -y
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip -O ~/ngrok.zip
+sudo unzip ~/ngrok.zip -d /usr/bin 
+rm -rf ~/ngrok.zip
+
+ngrok
