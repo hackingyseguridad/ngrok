134ad216e415e2af04e618f18c9f3d3c3d7fc8b3
hackingyseguridad <antonio_taboada@hotmail.es>
2020-09-28 13:31:27 +0200

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -4,3 +4,5 @@ sleep 2
 ./ngrok http 8080 > /dev/null 2>&1 &
 sleep 7
 ngrok_link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[0-9a-z]*\.ngrok.io")
+
+printf "\n \e[1;31m[\e[0m\e[1;37m~\e[0m\e[1;31m]\e[0m\e[1;96m Press Ctrl + C to exit.\e[0m\n"
