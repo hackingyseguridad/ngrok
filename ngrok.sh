511b26dbc9d489d00aab44a59e99afb2b44263cc
hackingyseguridad <antonio_taboada@hotmail.es>
2020-09-28 15:32:58 +0200

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -2,9 +2,10 @@
 service apache2 start
 sleep 2
 ./ngrok
-./ngrok http 8080 > /dev/null 2>&1 &
+./ngrok http 80 > /dev/null 2>&1 &
 sleep 7
-
+printf "\n \e[1;31m[\e[0m\e[1;37m~\e[0m\e[1;31m]\e[0m\e[1;96m La URL generada para acceso remoto desde internet es: \e[0m\n"
 curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[0-9a-z]*\.ngrok.io")
-
-printf "\n \e[1;31m[\e[0m\e[1;37m~\e[0m\e[1;31m]\e[0m\e[1;96m Press Ctrl + C to exit.\e[0m\n"
+printf "\n \e[1;31m[\e[0m\e[1;37m~\e[0m\e[1;31m]\e[0m\e[1;96m Las IP del servicio ngrok con las que establece el tunel son: \e[0m\n"
+ss |grep https
+printf "\n \e[1;31m[\e[0m\e[1;37m~\e[0m\e[1;31m]\e[0m\e[1;96m Tunel creado ...\e[0m\n"
